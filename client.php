52aa3bff1d889f312215f6078a70d46974d4b38d
Anarbek4646 <londonisthecapita66@gmail.com>
2021-04-26 19:51:38 +0600

    sss+

---
+++ client.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+</head>
+
+
+
+<body>
+
+  <form class="" action="mailto:info@londonappbrewery.com" method="post">
+    <label>Your Name:</label><br>
+    <input type="text" name="" value=""> <br>
+
+    <label>Your Email:</label> <br>
+    <input type="email" name="" value=""> <br>
+
+    <label>Your Message</label><br>
+    <textarea name="name" rows="10" cols="30"></textarea><br>
+  
+    <input type="submit" name="">
+  </form>
+    </body>
+
+</html>
